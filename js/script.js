e2e8b7b7122fef4e3d8062d8aebb01f20de96caa
SarahJD <sarahjdudek@gmail.com>
2020-12-31 11:53:52 +0100

    simplified toggle-function

--- js/script.js
+++ js/script.js
@@ -3,27 +3,7 @@ let shareIcon = document.querySelector('.share-icon');
 function toggleShare() {
     let shareList = document.querySelector('.share-list__container');
     let testimonialBox = document.querySelector('.testimonial__box');
-    shareList.classList.toggle('active');
-    testimonialBox.classList.toggle('inactive')
+    shareList.classList.toggle('show');
 }
 
 shareIcon.addEventListener ('click', toggleShare);
-
-
-// media query event handler
-if (matchMedia) {
-    const mq = window.matchMedia("(min-width: 475px)");
-    mq.addListener(WidthChange);
-    WidthChange(mq);
-    }
-    
-    // media query change
-    function WidthChange(mq) {
-    if (mq.matches) {
-        let testimonialBox = document.querySelector('.testimonial__box');
-        testimonialBox.classList.toggle('active')
-    } else {
-
-    }
-    
-    }
